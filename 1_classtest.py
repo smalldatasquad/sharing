98b2a7a159aade6b88de6420c3b725e048c1046e
melaniehoff <hoffmelanie@gmail.com>
2016-10-30 17:15:44 -0400

    Adding class test

---
+++ 1_classtest.py
@@ -0,0 +1,34 @@
+
+#!/usr/bin/python
+#
+# class JarLayer:
+# 	def __init__(self,input_size,output_size):
+# 		self.weights = np.random.rand( output_size, input_size ) * 2.0 - 1.0
+# 		self.bias    = np.zeros( ( output_size, 1 ) )
+#
+#
+#
+# class JarVisualizer:
+# 	def __init__(self,data_xmin,data_xmax,data_ymin,data_ymax,report_freq,buffer_size = 100):
+# 		# Stuff
+#
+# 	def openJar():
+#         return "blah"
+#
+
+class Jar:
+	def __init__(self, flavor):
+		# Create layers:
+		self.flavor = flavor
+
+	def pickle():
+        self.flavor = "pickled " + flavor
+
+    def getContents():
+        return self.flavor
+
+# USAGE
+
+foo = 5
+cucumber = new Jar("cucumber")
+radish = new Jar("radish")
